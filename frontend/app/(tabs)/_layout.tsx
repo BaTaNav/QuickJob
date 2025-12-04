04fb969decef68f261059ac40f608bd4ed979b81
BaTaNav <77064177+BaTaNav@users.noreply.github.com>
2025-12-04 14:43:57 +0100

    added homepage code to index.tsx (code made by denis)

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, SafeAreaView } from 'react-native';
-
-import Colors from '../../constants/Colors';
-
-import { useColorScheme } from '../../components/useColorScheme';
-import { useClientOnlyValue } from '../../components/useClientOnlyValue';
-import Login from '@/components/Login';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Tabs } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
-  function handleLogin(email: string, password: string): void {
-    throw new Error('Function not implemented.');
-  }
-
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
-      <Login onSubmit={handleLogin} title="Student Login" />
-    </SafeAreaView>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: useClientOnlyValue(false, true), // verberg headers op web
+        tabBarStyle: {
+          backgroundColor: Colors[colorScheme ?? 'light'].background,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarLabel: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Profile',
+          tabBarLabel: 'Profile',
+        }}
+      />
+    </Tabs>
   );
 }
