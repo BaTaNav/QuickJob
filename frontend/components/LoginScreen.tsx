620660926393ac4b2a47bea9cb2795b5666b5569
BaTaNav <77064177+BaTaNav@users.noreply.github.com>
2025-12-04 11:46:53 +0100

    Create LoginScreen.tsx
    added typescript interface

---
+++ frontend/components/LoginScreen.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
+
+
+
+interface LoginScreenProps {
+
+  onLoginSuccess: (role: string) => void; 
+}
