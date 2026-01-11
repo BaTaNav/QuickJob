2e86f64b6e86e48d07241cb2dcd6f0209630081e
DenisBuj <denis.bujorean@student.ehb.be>
2026-01-11 16:51:40 +0100

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,5 @@
-//const API_BASE = "http://192.168.129.7:3000";
-const API_BASE = "http://localhost:3000";
+const API_BASE = "http://192.168.129.7:3000";
+
 
 
 export async function apiPost<T>(path: string, body: unknown): Promise<T> {
